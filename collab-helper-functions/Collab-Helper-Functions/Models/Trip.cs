ac8b070d1eee340a7515d78c99367955896080a4
Annie Ellenberger <aellenberger@redrocktechsolutions.com>
2020-04-03 13:58:08 -0400

    Initial Solution with Trip table storage query function

---
+++ collab-helper-functions/Collab-Helper-Functions/Models/Trip.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collab_Helper_Functions.Models
+{
+    public class Trip : TableEntity
+    {
+        public DateTime TripDate { get; set; }
+        public string HelperName { get; set; }
+        public int HelperId { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+    }
+}
