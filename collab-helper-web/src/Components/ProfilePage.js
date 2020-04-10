3462fd00ca8135b6e50d1e8cbf58017a9e46e5cd
Barranger Ridler <barranger@gmail.com>
2020-04-09 20:39:21 -0400

    layout of profile

--- collab-helper-web/src/Components/ProfilePage.js
+++ collab-helper-web/src/Components/ProfilePage.js
@@ -1,31 +1,71 @@
 import React, { useContext } from "react";
 import { UserContext } from "../providers/UserProvider";
-import {auth, analytics} from "../firebase";
+import { Grid, Avatar, Typography, Button, ListSubheader } from "@material-ui/core";
+import { List, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
+import { auth, analytics } from "../firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from '../controls/Box';
+
+const tempNetwork = [
+  { name: "Barranger Ridler", email: "barranger@gmail.com" },
+  { name: "Annie Ellenberger", email: "annie.ellenberger@gmail.com"}
+]
+
+const useStyles = makeStyles(theme => ({
+  box: {
+    backgroundColor: "#bccbde",
+    padding: "2.5em",
+    marginTop: "5em",
+    borderRadius: 12
+  },
+  profilePic: {
+    width: 120,
+    height: 120
+  }
+}));
 
 const ProfilePage = () => {
   const user = useContext(UserContext);
-  const {photoURL, displayName, email} = user;
-  analytics.logEvent('Loading user page: ' + displayName);
-  
+  const { photoURL, displayName, email } = user;
+  const classes = useStyles();
+
+  analytics.logEvent("Loading user page: " + displayName);
+
   return (
-    <div className = "mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
-      <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
-        <div
-          style={{
-            background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
-            backgroundSize: "cover",
-            height: "200px",
-            width: "200px"
-          }}
-          className="border border-blue-300"
-        ></div>
-        <div className = "md:pl-4">
-        <h2 className = "text-2xl font-semibold">{displayName}</h2>
-        <h3 className = "italic">{email}</h3>
-        </div>
-      </div>
-      <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {auth.signOut()}}>Sign out</button>
-    </div>
-  ) 
+    <Grid container>
+      <Grid item xs={3}>
+        <Box>
+          <Avatar className={classes.profilePic} src={ photoURL } />
+          <Typography variant="h4">{displayName}</Typography>
+          <Typography variant="h6">{email}</Typography>
+          <Button onClick={() => auth.signOut()}>Sign out</Button>
+        </Box>
+      </Grid>
+      <Grid item xs={9}>
+        <Box>
+          
+        <Typography className={classes.text} variant="h4" component="h2">Collab(oration) Helper</Typography>
+      <List className={classes.root} subheader={<ListSubheader>People in your network</ListSubheader>}>
+        {tempNetwork.map( p => 
+          (<ListItem alignItems="flex-start" key={p.email}>
+            <ListItemAvatar>
+              <Avatar src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary={p.name}
+              secondary={
+                <React.Fragment>
+                  <Typography component="span" variant="body2" color="textPrimary">{p.email}</Typography>
+                </React.Fragment>
+              }
+            />
+          </ListItem>)
+      )}
+          </List>
+          <Button variant="contained" color="primary">Add New Contact</Button>
+          </Box>
+      </Grid>
+    </Grid>
+  );
 };
 export default ProfilePage;
