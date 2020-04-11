7a285c947b706cee54cfb383879a1cd7b6732f3d
Annie Ellenberger <aellenberger@redrocktechsolutions.com>
2020-04-11 10:22:56 -0400

    Implement SendGrid in function

--- collab-helper-functions/functions/index.js
+++ collab-helper-functions/functions/index.js
@@ -1,23 +1,51 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const cors = require('cors')({origin: true});
+const sgMail = require("@sendgrid/mail");
+const cors = require('cors')({ origin: true });
+
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+const path = require('path')
+require('dotenv').config()
+
+console.log('Process env: ', process.env);
+
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Cloud Functions
 
 exports.genericemail = functions.https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
-  res.status(200).send(`<!doctype html>
-    <head>
-      <title>Time</title>
-    </head>
-    <body>
-      ${'BONG '.repeat(hours)}
-    </body>
-  </html>`);
+  const { toEmail, fromEmail, name, email, phone, message } = req.body;
+  return cors(req, res, () => {
+    var text = `<div>
+      <h4>Information</h4>
+      <ul>
+        <li>
+          Name - ${name || ""}
+        </li>
+        <li>
+          Email - ${email || ""}
+        </li>
+        <li>
+          Phone - ${phone || ""}
+        </li>
+      </ul>
+      <h4>Message</h4>
+      <p>${message || ""}</p>
+    </div>`;
+    const msg = {
+      to: toEmail,
+      from: fromEmail,
+      subject: `Need anything? ${name} is going to the store soon!`,
+      text: text,
+      html: text
+    };
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.send(msg).then(() => {
+      res.status(200).send('{ "status": "success"}');
+      return res;
+    }).catch((err) => {
+      res.status(500).send('{ "status": "error", "errorMessage": "' + err + '"}');
+    });
+  });
 });
