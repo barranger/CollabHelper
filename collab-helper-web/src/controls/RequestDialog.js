66015bfe92ccbbd051e930d6c2049513119c2841
Barranger Ridler <barranger@gmail.com>
2020-04-16 09:17:24 -0400

    Interm commit

---
+++ collab-helper-web/src/controls/RequestDialog.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  whiteBg: {
+    backgroundColor: "white",
+  },
+  button: {
+    marginTop: 14,
+  },
+}));
+
+const RequestDialog = props => {
+
+  const {open, setOpen} = props;
+  const classes = useStyles();
+  return (
+    <Dialog
+    open={open}
+    onClose={() => setOpen(false)}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle className={classes.whiteBg} id="alert-dialog-title">
+      {"Request an item??"}
+    </DialogTitle>
+    <DialogContent className={classes.whiteBg}>
+      <DialogContentText id="alert-dialog-description">
+        By Requesting an item to be picked up for you, you are acknowledging
+        that you will pay for the item. Payment for the item must be done
+        between you and the person picking up the item, and will not be done
+        by this app.
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions className={classes.whiteBg}>
+      <Button onClick={() => setOpen(false)} color="primary">
+        Disagree
+      </Button>
+      <Button
+        onClick={() => {
+          setOpen(false);
+          props.requestItem();
+        }}
+        color="primary"
+        autoFocus
+      >
+        Agree
+      </Button>
+    </DialogActions>
+  </Dialog>
+  )
+}
+
+export default RequestDialog;
--- collab-helper-web/src/screens/RequestItem.js
+++ collab-helper-web/src/screens/RequestItem.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Typography, TextField, Button, Dialog } from "@material-ui/core";
+import { Typography, TextField, Button } from "@material-ui/core";
 import Box from "../controls/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import { getTripById, addItemToTrip } from "../services/tripService";
-import {  DialogActions, 
-          DialogContent, 
-          DialogContentText,
-          DialogTitle } from "@material-ui/core";
+import RequestDialog from "../controls/RequestDialog";
 import { UserContext } from "../providers/UserProvider";
 
 const useStyles = makeStyles((theme) => ({
@@ -21,14 +18,15 @@ const useStyles = makeStyles((theme) => ({
 const RequestItem = ({ tripId }) => {
   const [loaded, setLoaded] = useState(false);
   const [trip, setTrip] = useState({});
-  const [item, setItem] = useState();
-  const [open, setOpen] = React.useState(false);
+  const [item, setItem] = useState("");
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
-  
+
   const user = useContext(UserContext);
 
   const requestItem = () => {
-    console.log('I will add the item', item)
+    console.log("I will add the item", item);
+    
     addItemToTrip(user, tripId, item);
   };
   useEffect(() => {
@@ -41,49 +39,63 @@ const RequestItem = ({ tripId }) => {
     }
     getTrip();
   });
-  return [
-    <Box>
-      <form onSubmit={e => {e.preventDefault(); setOpen(true)}}>
-        <Typography>Request something from {trip.where}</Typography>
-        <Typography>on {trip.when}</Typography>
-        <TextField
-          value={item}
-          label="What do you need"
-          fullWidth
-          onChange={(e) => setItem(e.target.value)}
-        />
-        <Button
-          className={classes.button}
-          type="submit"
-          variant="contained"
-          color="primary"
+
+  const myTrip = trip.uid === user.uid;
+  const alreadyReq =
+    trip.items &&
+    trip.items.filter((i) => i.user && i.user.uid === user.uid).length > 0;
+
+  return (
+    <div>
+      <Box>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setOpen(true);
+          }}
         >
-          Request Item
-        </Button>
-      </form>
-    </Box>,
-    <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-      <DialogTitle className={classes.whiteBg} id="alert-dialog-title">
-        {"Request an item??"}
-      </DialogTitle>
-      <DialogContent className={classes.whiteBg}>
-        <DialogContentText id="alert-dialog-description">
-          By Requesting an item to be picked up for you, you are acknowledging
-          that you will pay for the item. Payment for the item must be done
-          between you and the person picking up the item, and will not be done
-          by this app.
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions className={classes.whiteBg}>
-        <Button onClick={() => setOpen(false)} color="primary">
-          Disagree
-        </Button>
-        <Button onClick={() => { setOpen(false); requestItem() }} color="primary" autoFocus>
-          Agree
-        </Button>
-      </DialogActions>
-    </Dialog>,
-  ];
+          <Typography>Request something from {trip.where}</Typography>
+          <Typography>on {trip.when}</Typography>
+
+          {!myTrip && !alreadyReq && (
+            <>
+              <TextField
+                value={item}
+                label="What do you need"
+                fullWidth
+                onChange={(e) => setItem(e.target.value)}
+              />
+              <Button
+                className={classes.button}
+                disabled={!item || item.length === 0}
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                Request Item
+              </Button>
+            </>
+          )}
+
+          {myTrip && (
+            <>
+              <Typography>
+                Here are the Items people have requested for you to pick up
+              </Typography>
+              <ul>
+                {trip.items.map((i) => (
+                  <li>
+                    {i.user.displayName}: {i.what}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </form>
+      </Box>
+      <RequestDialog open={open} setOpen={setOpen} requestItem={requestItem} />
+    </div>
+  );
 };
 
 export default RequestItem;
